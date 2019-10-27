19e336db2363cc07bdb65197cbb9835489d10f47
Allen Mi <allen.mi@yale.edu>
2019-10-27 07:36:28 -0400

    RC1

--- mindmap.py
+++ mindmap.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import webbrowser
 
 from google.cloud.speech_v1 import enums
 
@@ -10,6 +11,9 @@ from utils.py_to_json import generate_json_pair
 from utils.knit import knit
 
 def run(gs_file_uri, title, num_keywords, searchword, dry_run=False, dry_run_file=None):
+
+    print("[MindMap-Main][0/6] Waiting for dictation to complete...")
+
     if not dry_run:
         dictation_parts = async_dictate(gs_file_uri, enums.RecognitionConfig.AudioEncoding.FLAC, 16000, "en-US")
         dictation = ''.join(dictation_parts)
@@ -17,31 +21,24 @@ def run(gs_file_uri, title, num_keywords, searchword, dry_run=False, dry_run_fil
         assert(dry_run_file != None)
         with open(dry_run_file) as f:
             dictation = f.read()
-    
-    print(dictation)
 
-    print("====================")
+    print("[MindMap-Main][1/6] Dictation received, sanitizing...")
 
     sanitized_dictation = sanitize(dictation)
 
-    print(sanitized_dictation)
-
-    print("====================")
+    print("[MindMap-Main][2/6] Generating "+str(num_keywords)+" keyword-weight pairs via TextRank...")
 
     kw_w_pairs = textrank(sanitized_dictation, topn=num_keywords)
-    print(kw_w_pairs)
 
-    print("====================")
+    print("[MindMap-Main][3/6] Calculating similarity between keywords...")
 
     sim_mat = similarity(kw_w_pairs)
-    print(sim_mat)
 
-    print("====================")
+    print("[MindMap-Main][4/6] Matching search term to keywords...")
 
     (match_kw, match_score) = closest_match(kw_w_pairs, searchword)
-    print(match_kw, match_score)
 
-    print("====================")
+    print("[MindMap-Main][5/6] Preparing for output...")
 
     (nodes_list, edges_list) = generate_json_pair(kw_w_pairs, sim_mat)
 
@@ -59,4 +56,9 @@ def run(gs_file_uri, title, num_keywords, searchword, dry_run=False, dry_run_fil
 
     knit(script_path+"/index.html", script_path+"/"+title+".html", replacement_map)
 
-run(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4], dry_run=True, dry_run_file="./resources/how_trees_bend_the_law_of_physics.txt")
+    print("[MindMap-Main][6/6] Output completed. Opening in web browser...")
+
+    url = "file://"+script_path+"/"+title+".html"
+    webbrowser.open(url, new=2) 
+
+run(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4], dry_run=False)
--- start-mindmap.sh
+++ start-mindmap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## Mindmap: To Transcribe, Summarize and Visualize
+## MindMap: To Transcribe, Visualize, and Inspire
 ## Authors: floor people, chair people, bed people
+VERSION=1.0rc1
 
 # Dependencies: youtube-dl, sox, python3, google-cloud-sdk(gsutil, gcloud, etc.), spacy, nltk, textacy, numpy
 
@@ -15,11 +16,22 @@ PYTHON3_PATH=/home/allen/Programs/anaconda3/envs/mindmap/bin/python
 GSUTIL_PATH=/usr/bin/gsutil
 MINDMAP_PATH=./mindmap.py
 
+echo "MindMap: To Transcribe, Visualize, and Inspire"
+echo "Authors: floor people, chair people, bed people"
+echo "Version: ${VERSION}"
+echo
+echo "Please name your project:"
+read TITLE
+echo "Please enter the video URL:"
+read VIDEO_URL
+echo "Please enter the number of keywords:"
+read NUM_KEYWORDS
+echo "Please enter a term you would like to search in the video:"
+read SEARCHWORD
+
 # Specify video and audio URI/path, create temp directory
-VIDEO_URL=$1
-TITLE=$2
-NUM_KEYWORDS=$3
-SEARCHWORD=$4
+echo
+echo "[MindMap] Preparing..."
 TMP_DIR_PATH=$(mktemp -d -t mindmap-video-XXXXXXXXXX)
 DOWNLOADED_AUDIO_PATH="${TMP_DIR_PATH}/downloaded-audio"
 CONVERTED_AUDIO_PATH="${TMP_DIR_PATH}/converted-audio.flac"
@@ -28,11 +40,17 @@ AUDIO_GS_UUID=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
 AUDIO_GS_URI="${CLOUD_STORE_BUCKET}/${AUDIO_GS_UUID}.flac"
 
 # Download audio of the video specified, store in FLAC encoding
+echo
+echo "[MindMap] Downloading audio..."
 ${YOUTUBE_DL_PATH} ${VIDEO_URL} -x --audio-format "wav" -o "${DOWNLOADED_AUDIO_PATH}.%(ext)s"
 ${SOX_PATH} "${DOWNLOADED_AUDIO_PATH}.wav" --rate 16k --bits 16 --channels 1 ${CONVERTED_AUDIO_PATH}
 
 # Upload audio file to cloud storage
+echo
+echo "[MindMap] Uploading audio to Google Cloud..."
 ${GSUTIL_PATH} cp ${CONVERTED_AUDIO_PATH} "${AUDIO_GS_URI}"
 
 # Call Mindmap
+echo
+echo "[MindMap] Invoking main program..."
 ${PYTHON3_PATH} ${MINDMAP_PATH} "${AUDIO_GS_URI}" "${TITLE}" "${NUM_KEYWORDS}" "${SEARCHWORD}"

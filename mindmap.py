dbcc444017bcde65ace7448b710a3d74f4edcc6e
Allen Mi <allen.mi@yale.edu>
2019-10-26 23:20:25 -0400

    Text segmentation and cleaning

--- mindmap.py
+++ mindmap.py
@@ -7,8 +7,6 @@ def run(gs_file_uri):
     print(gs_file_uri)
     dictation = async_dictate(gs_file_uri, enums.RecognitionConfig.AudioEncoding.FLAC, 16000, "en-US")
 
-    for d in dictation:
-        print(d)
-        print()
+    print(''.join(dictation))
 
 run(sys.argv[1])

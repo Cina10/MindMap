40856f33a14996c9cfd500fda5da3ba2a78478b7
Allen Mi <allen.mi@yale.edu>
2019-10-27 05:26:11 -0400

    VIsualize graph

--- utils/knit.py
+++ utils/knit.py
@@ -7,5 +7,3 @@ def knit(template_path, store_path, replacement_map):
     
     with open(store_path, 'w') as f:
         f.write(html)
-
-knit("../index.html", "../test.html", {"BLAHBLAHBLAH": "NININI"})
